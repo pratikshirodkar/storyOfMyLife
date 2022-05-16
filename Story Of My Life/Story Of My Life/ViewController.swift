edea3a3595c68a58a81ff355d3f89985e926825b
pratikshirodkar <30596636+pratikshirodkar@users.noreply.github.com>
2022-05-16 16:02:18 +0530

    initial commit via web

---
+++ Story Of My Life/Story Of My Life/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Story Of My Life
+//
+//  Created by Pratik Shirodkar on 16/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
